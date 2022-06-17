67e40d1c9a2433813d4a1214680843966dcc4eac
Charlie Bright <charliebright@hotmail.co.uk>
2022-06-17 13:16:16 +0100

    + Updated comments for clarity

--- svPost/svPost-flows.sh
+++ svPost/svPost-flows.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
+# Locate create-flow-files installation.
 generate-flows=/home/USER/apps/create-flow-files
 
-# Set the converter options.
+# Set the in/out directories.
 meshDir=/home/USER/Documents/sim-project/simulation/mesh-complete/mesh-surfaces
 resDir=/home/USER/Documents/sim-project/simulation/N-procs_case_svPost
 outputDir=/home/USER/Documents/sim-project/simulation/N-procs_case_svPost-flows
 
+# Set the converter options.
 singleFile=no
 skipWalls=yes
 units=cm
 
 # Call the converter.
-createFlows --mesh-directory ${meshDir} --results-directory ${resDir} --output-directory ${outputDir} --single-file ${singleFile} --skip-walls ${skipWalls} --units ${units}
+generate-flows --mesh-directory ${meshDir} --results-directory ${resDir} --output-directory ${outputDir} --single-file ${singleFile} --skip-walls ${skipWalls} --units ${units}
--- svPost/svPost.sh
+++ svPost/svPost.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
+# Locate svPost installation.
 svpost=/usr/local/sv/svsolver/DATE/svpost
 
+# Set the desired timesteps.
 start=10
 stop=100
 incr=10
 
+# Set the in/out directories.
 indir=/home/USER/Documents/sim-project/simulation/N-procs_case
 outdir=/home/USER/Documents/sim-project/simulation/N-procs_case_svPost
 
+# Run svPost. SELECT ONE and comment out/delete the other!:
+# Generate 1 file per timestep.
 $svpost -indir ${indir} -outdir ${outdir} -start ${start} -stop ${stop} -incr ${incr} -vtp all_results -vtu all_results
+# Generate 1 .vtu and 1 .vtp file containing all timesteps.
+$svpost -indir ${indir} -outdir ${outdir} -start ${start} -stop ${stop} -incr ${incr} -all -vtkcombo -vtu all_results -vtp all_results
